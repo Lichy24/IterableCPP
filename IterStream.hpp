fc106e56cf4a21dcebfdc0705f46bec643f71cfa
Adi Lichy <adi_lichy@yahoo.com>
2019-05-28 01:24:39 +0300

    test

--- IterStream.hpp
+++ IterStream.hpp
@@ -8,27 +8,34 @@
 * we can control how it will display the values of incoming input
 */
 namespace itertools {
+	template<typename Iterable>
+	string iterable_to_string(const Iterable& iterable) {
+		ostringstream ostr;
+		for (auto i : iterable)
+			ostr << i << " ";
+		return ostr.str();
+	}
+
+	template <typename T, typename U>
+	std::ostream& operator<<(std::ostream & os,const std::pair<T,U> &p) {//overload pair output stream
+		os << p.first << "," << p.second;
+		return os;
+	}
 	template <typename K>
 	std::ostream& operator<<(std::ostream & os, const std::vector<K> &vec) {//overload vector output stream
-		
+
 		os << "{";
 		auto iter = vec.begin();
 		if (iter != vec.end()) {
 			os << *iter;
 			++iter;
 		}
-		while (iter!=vec.end())
+		while (iter != vec.end())
 		{
 			os << "," << *iter;
 			++iter;
 		}
 		os << "}";
-		return os;
-	}
-
-	template <typename T, typename U>
-	std::ostream& operator<<(std::ostream & os,const std::pair<T,U> &p) {//overload pair output stream
-		os << p.first << "," << p.second;
 		return os;
 	}
 }
